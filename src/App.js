52a34c7317479401b04aea96c91795ef3eeb435a
randomgamer800 <73993052+randomgamer800@users.noreply.github.com>
2022-01-23 14:02:05 +0800

    first commit (App.js)
    Add background to all the pages on app
    added a navbar route for testing (will delete when completed)

--- src/App.js
+++ src/App.js
@@ -6,7 +6,19 @@ import Dashboard from "./pages/dashboard";
 import Guides from "./pages/guides";
 import Rewards from "./pages/rewards";
 import Signup from "./pages/signup";
+import Navbar from "./components/Layout/Navbar"; //for testing
 import { UserAuthContextProvider } from "./util/userAuthContext";
+//Background Image
+import Background from "./images/Background2.jpg";
+
+
+var backgroundStyle = {
+  minHeight: "100vh", 
+  backgroundImage: `url(${Background})`,
+  backgroundSize: "150%", 
+  backgroundPosition: "center",
+  backgroundRepeat: "repeat-y",
+};
 
 //for routing 
 //https://www.youtube.com/watch?v=Law7wfdg_ls
@@ -14,15 +26,19 @@ function App() {
   return (
       <Router>
         <div className="App">
-          <UserAuthContextProvider>
-          <Routes>
-            <Route exact path="/" element={<Login />} />
-            <Route exact path="/signup" element={<Signup />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/guides" element={<Guides />} />
-            <Route path="/rewards" element={<Rewards />} />
-          </Routes>
-          </UserAuthContextProvider>
+          <div className="container" style={backgroundStyle}>
+            <UserAuthContextProvider>
+              <Routes>
+                <Route exact path="/" element={<Login />} />
+                <Route exact path="/signup" element={<Signup />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/guides" element={<Guides />} />
+                <Route path="/rewards" element={<Rewards />} />
+                
+                <Route path="/navbartest" element={<Navbar />} /> {/*to delete after test*/}
+              </Routes>
+            </UserAuthContextProvider>
+          </div>
         </div>
       </Router>
   );
