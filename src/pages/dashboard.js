6c0bbce348488fbf23b1e433849b9b23f42d5240
randomgamer800 <73993052+randomgamer800@users.noreply.github.com>
2022-01-25 20:11:03 +0800

    2nd commit (dashboard.js)
    added coloured background for content and some content inside for testing (might refine this soon)

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,7 +1,32 @@
-const Dashboard = () => {
-    return (
-        console.log("Hello")
-    );
+import React from "react";
+import { Grid, TextField, Button, Typography } from "@material-ui/core";
+
+export default function Dashboard() {
+  return (
+    <Grid
+      container
+      justify="center"
+      alignItems="center"
+      direction="column"
+      style={{ minHeight: "100vh", marginTop:40, marginBottom:40 }}
+      spacing={5}
+    >
+      <Grid item style={{ border: "0.2px solid gray", backgroundColor:"#FFE5B4" }}>
+        <Content/>
+      </Grid>
+    </Grid>
+  );
 }
 
-export default Dashboard;
+const Content = () => {
+  return (
+    <Grid container direction="column" alignItems="center" justify="center">
+        <Typography variant="h3" color="primary">
+          Dashboard
+        </Typography>
+        <Typography variant="h5" color="primary">
+        Welcome to our homepage!
+        </Typography>
+    </Grid>
+  );
+};
