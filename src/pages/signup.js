755a0195db79b9a76cb45b6b2ba432ab2747a14f
randomgamer800 <73993052+randomgamer800@users.noreply.github.com>
2022-02-01 15:09:51 +0800

    Signup.js
    Combined backend code with frontend

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,61 +1,64 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { Form, Alert } from "react-bootstrap";
-import { Button } from "react-bootstrap";
+//import { Button } from "react-bootstrap"; first commit removal
 import { useUserAuth } from "../util/userAuthContext";
+import { auth, db } from '../util/firebase'
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import {Grid,Paper,Avatar,TextField,Box} from "@material-ui/core/";
+import LockIcon from '@mui/icons-material/Lock';
+import "./login.css"
 
-const Signup = () => {
-  const [email, setEmail] = useState("");
-  const [error, setError] = useState("");
-  const [password, setPassword] = useState("");
-  const { signUp } = useUserAuth();
+
+const Signup = (props) => {
+  // defining state
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
   let navigate = useNavigate();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setError("");
-    try {
-      await signUp(email, password);
-      navigate("/");
-    } catch (err) {
-      setError(err.message);
-    }
-  };
+  // signup
+  const signup = (e) => {
+      e.preventDefault();
+      auth.createUserWithEmailAndPassword(email, password).then((cred) => {
+          db.collection('SignedUpUsersData').doc(cred.user.uid).set({
+              Email: email,
+              // Password: password
+          }).then(() => {
+              setEmail('');
+              setPassword('');
+              setError('');
+              navigate("/")
+          }).catch(err => setError(err.message));
+      }).catch(err => setError(err.message));
+  }
 
-  return (
-    <>
-      <div className="p-4 box">
-        <h2 className="mb-3">Signup</h2>
-        {error && <Alert variant="danger">{error}</Alert>}
-        <Form onSubmit={handleSubmit}>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Control
-              type="email"
-              placeholder="Email address"
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </Form.Group>
-
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Control
-              type="password"
-              placeholder="Password"
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </Form.Group>
-
-          <div className="d-grid gap-2">
-            <Button variant="primary" type="Submit">
-              Sign up
-            </Button>
-          </div>
-        </Form>
+    //can add Signup successful
+    return (
+      <div className='sign'>
+          <br />
+          <h2>Sign up</h2>
+          <br />
+          <form autoComplete="off" className='form-group' onSubmit={signup}>
+              <label htmlFor="email">Email</label>
+              <input type="email" className='form-control' required
+                  onChange={(e) => setEmail(e.target.value)} value={email} />
+              <br />
+              <label htmlFor="passowrd">Password</label>
+              <input type="password" className='form-control' required
+                  onChange={(e) => setPassword(e.target.value)} value={password} />
+              <br />
+              <button type="submit" className='btn btn-success btn-md mybtn'>SUBMIT</button>
+          </form>
+          {error && <span className='error-msg'>{error}</span>}
+          <br />
+          <span>
+              <Link to="/"> Already have an account? Login</Link>
+          </span>
       </div>
-      <div className="p-4 box mt-3 text-center">
-        Already have an account? <Link to="/">Log In</Link>
-      </div>
-    </>
-  );
-};
+  )
+}
 
 export default Signup;
