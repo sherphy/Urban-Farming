d0db9919abf1c1a9bd754bad14cf37475e3817a3
sherphy <sherphy.k@gmail.com>
2022-02-02 02:59:50 +0800

    notifs for "already added in cart" too

--- src/pages/rewards.js
+++ src/pages/rewards.js
@@ -67,7 +67,7 @@
 //     useEffect(()=>{
 //         getProducts();
 //     },[])
-    
+
 //     let Product;
 //     const addToCart = (product) => {
 //         if ( useremail !== null) {
@@ -82,9 +82,9 @@
 //         else{
 //             props.history.push('/dashboard');
 //         }
-        
+
 //     }
-    
+
 //     return (
 //         <>
 //             {/* <Navbar user={user}/>            */}
@@ -108,9 +108,9 @@
 // export default Rewards;
 
 //BASICALLY NEED TO FIND A WAY TO ADD MORE THAN 1 
-import React,{useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import Products from '../util/Product'
-import {auth, db } from '../util/firebase'
+import { auth, db } from '../util/firebase'
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -119,90 +119,104 @@ toast.configure();
 const Rewards = (props) => {
 
     // getting current user uid
-    function GetUserUid(){
-        const [uid, setUid]=useState(null);
-        useEffect(()=>{
-            auth.onAuthStateChanged(user=>{
-                if(user){
+    function GetUserUid() {
+        const [uid, setUid] = useState(null);
+        useEffect(() => {
+            auth.onAuthStateChanged(user => {
+                if (user) {
                     setUid(user.uid);
                 }
             })
-        },[])
+        }, [])
         return uid;
     }
 
     const uid = GetUserUid();
 
     // getting current user function
-    function GetCurrentUser(){
-        const [user, setUser]=useState(null);
-        useEffect(()=>{
-            auth.onAuthStateChanged(user=>{
-                if(user){
-                    db.collection('SignedUpUsersData').doc(user.uid).get().then(snapshot=>{
+    function GetCurrentUser() {
+        const [user, setUser] = useState(null);
+        useEffect(() => {
+            auth.onAuthStateChanged(user => {
+                if (user) {
+                    db.collection('SignedUpUsersData').doc(user.uid).get().then(snapshot => {
                         setUser(snapshot.data().FullName);
                     })
                 }
-                else{
+                else {
                     setUser(null);
                 }
             })
-        },[])
+        }, [])
         return user;
     }
 
     const user = GetCurrentUser();
     console.log(user);
-    
+
     // state of products
-    const [products, setProducts]=useState([]);
+    const [products, setProducts] = useState([]);
 
     // getting products function
-    const getProducts = async ()=>{
-        const products = await db.collection('Products').orderBy("price","asc").get();
+    const getProducts = async () => {
+        const products = await db.collection('Products').orderBy("price", "asc").get();
         const productsArray = [];
-        for (var snap of products.docs){
+        for (var snap of products.docs) {
             var data = snap.data();
             data.ID = snap.id;
             productsArray.push({
                 ...data
             })
-            if(productsArray.length === products.docs.length){
+            if (productsArray.length === products.docs.length) {
                 setProducts(productsArray);
             }
         }
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         getProducts();
-    },[])
-    
+    }, [])
+
     let Product;
-    const addToCart = (product)=>{
-        if(uid!==null){
+    const addToCart = (product) => {
+        if (uid !== null) {
             console.log(product);
-            Product=product;
-            Product['qty']=1;
-            Product['TotalProductPrice']=Product.qty*Product.price;
-            db.collection('Cart ' + uid).doc(product.ID).set(Product).then(()=>{
-            console.log('successfully added to cart');
-            //success notification procs even when quantity is more than 1 
-            toast.success('This product has been added to your cart', {
-                position: "top-right",
-                autoClose: 2000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: false,
-                draggable: false,
-                progress: undefined,
-            });
+            Product = product;
+            Product['qty'] = 1;
+            Product['TotalProductPrice'] = Product.qty * Product.price;
+            db.collection('Cart ' + uid).doc(product.ID).set(Product).then(() => {
+                console.log('successfully added to cart');
+                //success notification procs even when quantity is more than 1 
+                //can uncomment once i find a way to increase qty from rewards itself
+
+                // if (Product.qty !== 1) {
+                    toast.success('This product has been added to your cart', {
+                        position: "top-right",
+                        autoClose: 2000,
+                        hideProgressBar: false,
+                        closeOnClick: true,
+                        pauseOnHover: false,
+                        draggable: false,
+                        progress: undefined,
+                    });
+                // }
+
+                // toast.warn('This product is already in your cart', {
+                //     position: "top-right",
+                //     autoClose: 2000,
+                //     hideProgressBar: false,
+                //     closeOnClick: true,
+                //     pauseOnHover: false,
+                //     draggable: false,
+                //     progress: undefined,
+                // });
             })
         }
-        else{
+        else {
             props.history.push('/dashboard');
         }
     }
-    
+
     return (
         <>
             <br></br>
@@ -211,7 +225,7 @@ const Rewards = (props) => {
                     <h1 className='text-center'>Rewards</h1>
                     <h3 className='text-center'>Redeem real life items here!</h3>
                     <div className='products-box'>
-                        <Products products={products} addToCart={addToCart}/>
+                        <Products products={products} addToCart={addToCart} />
                     </div>
                 </div>
             )}
