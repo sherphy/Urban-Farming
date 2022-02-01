51eb7e1a320ec4fd38ea8f2d76ac13dc2e06d04b
randomgamer800 <73993052+randomgamer800@users.noreply.github.com>
2022-02-01 14:57:19 +0800

    dashboard.js
    edited dashboard.js code for testing and to make it more reponsive

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,32 +1,90 @@
+//contains the live camera viewable on the homepage
+
+// const Dashboard = () => (
+//     <div>
+//         <h1> Welcome back, </h1>
+//         <h3> Timelapse </h3>
+//     </div>
+// );
+
+//for user database
+// import firebase from "../util/firebase";
+// import React, { useState } from "react";
+
 import React from "react";
-import { Grid, TextField, Button, Typography } from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles, useTheme } from "@material-ui/styles";
+import "./dashboard.css"
 
-export default function Dashboard() {
-  return (
-    <Grid
-      container
-      justify="center"
-      alignItems="center"
-      direction="column"
-      style={{ minHeight: "100vh", marginTop:40, marginBottom:40 }}
-      spacing={5}
-    >
-      <Grid item style={{ border: "0.2px solid gray", backgroundColor:"#FFE5B4" }}>
-        <Content/>
-      </Grid>
-    </Grid>
-  );
-}
 
-const Content = () => {
+const useStyles = makeStyles((theme) => ({
+    title: {
+    marginTop: 0,
+    marginBottom: 10,
+  },
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    margin: "8px",
+    padding: "20px 20px 20px 20px",
+    background: "rgba(250, 243, 221, 0.85)",
+    borderRadius: "10px",
+  },
+  bodyText: {
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  
+}));
+
+function Dashboard() {
+  const classes = useStyles();
+
+      //for user database
+    // const [users, setUsers] = useState([]);
+    // const [loading, setLoading] = useState(false);
+
+    // const ref = firebase.firestore().collection("users");
+
+    // if (loading) {
+    //     return <h1>Loading...</h1>;
+    // }
+  
   return (
-    <Grid container direction="column" alignItems="center" justify="center">
-        <Typography variant="h3" color="primary">
-          Dashboard
-        </Typography>
-        <Typography variant="h5" color="primary">
-        Welcome to our homepage!
-        </Typography>
-    </Grid>
+    
+    <div className={classes.container}>
+
+          <Typography align="center" variant="h3" fontWeight="fontWeightBold" className={classes.title}>Dashboard</Typography>
+          
+          <Typography inline variant="h5" className={classes.bodyText}>
+          Welcome to our app! Here you can take a look at your plants.
+          </Typography>
+
+          {/* {users.map((user) => (
+              <div key={user.name}>
+                <h2>{user.points}</h2>
+              </div>
+            ))} */}
+          
+          <Typography inline variant="h4" align="center">Camera 1</Typography>
+          <div class="responsive">
+          <iframe src="https://www.youtube.com/embed/HpdO5Kq3o7Y?playlist=HpdO5Kq3o7Y&autoplay=1&mute=1&loop=1"></iframe> 
+          </div>
+
+          <Typography inline variant="h4" align="center">Camera 2</Typography>
+          <div class="responsive">
+          <iframe src="https://www.youtube.com/embed/K_Vg94nBiaY?playlist=K_Vg94nBiaY&autoplay=1&mute=1&loop=1"></iframe>
+          </div>
+
+          <Typography inline variant="h4" align="center">Camera 3</Typography>
+          <div class="responsive">
+          <iframe src="https://www.youtube.com/embed/XWq5kBlakcQ?playlist=XWq5kBlakcQ&autoplay=1&mute=1&loop=1"></iframe>
+          </div>
+          
+    </div>
   );
 };
+
+export default Dashboard
