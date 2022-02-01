ce34792ae525e7507093ddf2518f11d4a320f70a
randomgamer800 <73993052+randomgamer800@users.noreply.github.com>
2022-02-01 14:47:32 +0800

    rewards.js
    edited rewards.js to style the frontend basic shop features to suit the design of app

--- src/pages/rewards.js
+++ src/pages/rewards.js
@@ -1,33 +1,88 @@
-import React from "react";
-import { Grid, TextField, Button, Typography } from "@material-ui/core";
+//handles the frontend and backend for the reward shop
+import { Typography } from "@material-ui/core";
+import React, { useContext } from "react";
+import { makeStyles, useTheme } from "@material-ui/styles";
+import useWindowDimensions from "../util/useWindowDimensions";
+import shop from "../images/shop.png";
+import Box from "@material-ui/core/Box";
+import { ProductsContext } from '../util/ProductsContext'
+import { CartContext } from '../util/CartContext'
 
-export default function Rewards() {
+//frontend
+const useStyles = makeStyles((theme) => ({
+  title: {
+    marginTop: 0, 
+    marginBottom: 10,
+  },
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    margin: "8px",
+    padding: "20px 20px 20px 20px",
+    background: "rgba(250, 243, 221, 0.85)",
+    borderRadius: "10px",
+  },
+  bodyText: {
+    marginTop: 10,
+    marginBottom: 10,
+  },
+}));
+
+//backend
+const Products = () => {
+
+  const { products } = useContext(ProductsContext);
+
+  const { dispatch } = useContext(CartContext);
+
+  //frontend
+  const { height, width } = useWindowDimensions();
+  const classes = useStyles();
+  const theme = useTheme();
+
+  let stlWidth, stlHeight;
+  let imgWidth, imgHeight;
+  if (width <= 960) {
+    stlWidth = 250;
+    stlHeight = 250;
+    imgWidth = 200;
+    imgHeight = 200;
+  } else {
+    stlWidth = 400;
+    stlHeight = 400;
+    imgWidth = 350;
+    imgHeight = 350;
+  }
+
   return (
-    <Grid
-      container
-      justify="center"
-      alignItems="center"
-      direction="column"
-      style={{ minHeight: "100vh", marginTop:40, marginBottom:40 }}
-      spacing={5}
-    >
-      <Grid item style={{ border: "0.2px solid gray", backgroundColor:"#FFE5B4" }}>
-        <Content/>
-      </Grid>
-    </Grid>
-  );
+    <>
+    <div className={classes.container}>
+     <img src={shop} alt="Logo" style={{ height: imgHeight, width: imgWidth }}/>
+        {products.length !== 0 && <h1>Reward Shop</h1>}
+        <h3>Redeem your points for real life items here!</h3> 
+        <div className='products-container'>
+            {products.length === 0 && <div>Loading</div>}
+            {products.map(product => (
+                <div className='product-card' key={product.ProductID}>
+                {/* need to add icreasing price view */}
+                    <div className='product-img'>
+                        <img src={product.ProductImg} alt="not found" />
+                    </div>
+                    <div className='product-name'>
+                        {product.ProductName}
+                    </div>
+                    <div className='product-price'>
+                        {product.ProductPrice} points
+                </div>
+                    <button className='addcart-btn' onClick={() => dispatch({ type: 'ADD_TO_CART', id: product.ProductID, product })}>ADD TO CART</button>
+                </div>
+            ))}
+        </div>
+    </div>
+    </>
+)
 }
 
-const Content = () => {
-  return (
-    <Grid container direction="column" alignItems="center" justify="center">
-        <Typography variant="h3" color="primary">
-          Rewards
-        </Typography>
-        <Typography variant="h5" color="primary">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris rutrum rhoncus diam, quis porttitor nulla ultricies quis. Pellentesque lacinia imperdiet diam nec convallis. Fusce congue suscipit dui eu vehicula. Fusce viverra diam dolor, quis rhoncus felis cursus eget. In dolor ex, lobortis eget viverra eu, venenatis vitae orci. Ut auctor interdum tortor eu aliquet. In mattis accumsan dignissim. Ut vehicula eget velit non faucibus.
-        Sed magna lorem, aliquet eu porttitor quis, egestas vitae justo. Suspendisse ornare ipsum consequat lectus sodales iaculis. Etiam in pharetra nisi. Sed consequat lectus vel rhoncus dictum. Mauris vel laoreet leo. Aliquam erat volutpat. Nullam pellentesque in est in eleifend. Fusce at ex in lorem consectetur suscipit. Nam leo dui, interdum at velit et, tristique condimentum diam. Nullam at neque mauris. Quisque ut nisi leo. Curabitur vitae risus non turpis mattis porttitor.
-        </Typography>
-    </Grid>
-  );
-};
+export default Products
