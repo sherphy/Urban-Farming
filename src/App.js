c99b39e27cfb4edba79d4fdb057abda7426451ca
randomgamer800 <73993052+randomgamer800@users.noreply.github.com>
2022-02-01 15:05:33 +0800

    App.js Update
    changes to app.js to include navbar at needed pages and to reflect backend changes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
+//necessary
+import React, { Component } from 'react';
+import { auth, db } from "./util/firebase";
 //basic routing
-import { BrowserRouter as Router, Routes, Route, Link, useLocation } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 //for pages
 import Login from "./pages/login"
 import Dashboard from "./pages/dashboard";
 import Guides from "./pages/guides";
 import Rewards from "./pages/rewards";
+import Products from "./pages/rewards";
 import Signup from "./pages/signup";
+import Cart from "./pages/cart";
+//might need to remove later to hide it from view
+// import AddProducts from "./components/AddProducts";
+//utilities
 import { UserAuthContextProvider } from "./util/userAuthContext";
+import { ProductsContextProvider } from "./util/ProductsContext";
+import { CartContextProvider } from "./util/CartContext";
+import './index.css'
 //Background Image
 import Background from "./images/Background2.jpg";
 //Navbar
 import WithoutNav from "./components/Layout/WithoutNav";
 import WithNav from "./components/Layout/WithNav";
 
-import Notifications from "./components/Notifications"
+//testing
+import Timer from "./components/Timer"
 
 
 var backgroundStyle = {
@@ -26,8 +38,36 @@ var backgroundStyle = {
 
 //for routing 
 //https://www.youtube.com/watch?v=Law7wfdg_ls
-function App() {
-  return (
+export class App extends Component {
+    //for auth
+//   state = {
+//     user: null,
+// }
+
+// componentDidMount() {
+
+    // getting user info for navigation bar
+    //think can do something to this part to link them to different timelapses
+//     auth.onAuthStateChanged(user => {
+//         if (user) {
+//             db.collection('SignedUpUsersData').doc(user.uid).get().then(snapshot => {
+//                 this.setState({
+//                     user: snapshot.data()
+//                 })
+//             })
+//         }
+//         else {
+//             this.setState({
+//                 user: null
+//             })
+//         }
+//     })
+
+// }
+  render() {
+    return (
+      <ProductsContextProvider>
+      <CartContextProvider>
       <Router>
         <div className="App">
           <div className="container" style={backgroundStyle}>
@@ -41,16 +81,21 @@ function App() {
               
                 <Route path="/dashboard" element={<Dashboard />} />
                 <Route path="/guides" element={<Guides />} />
-                <Route path="/rewards" element={<Rewards />} />
+                <Route path="/rewards" element={<Products />} />
+                {/* might need to remove later to hide it from view */}
+                {/* <Route path='/addproducts' element={<AddProducts />} /> */}
               </Route>
         
-                <Route path="/notifications" element={<Notifications />} /> 
+               <Route path="/timer" element={<Timer />} /> {/*for testing/quiz component to be used later*/}
               </Routes>
             </UserAuthContextProvider>
           </div>
         </div>
       </Router>
+      </CartContextProvider>
+      </ProductsContextProvider>
   );
+ }
 }
 
 export default App;
