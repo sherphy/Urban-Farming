4cb7632583903a3e8acad67f6cc2258b327f8079
randomgamer800 <73993052+randomgamer800@users.noreply.github.com>
2022-02-18 20:34:54 +0800

    Navbar.js
    added button for cart,addproducts

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -63,6 +63,8 @@ const Navbar = () => {
                   <Button variant="contained" size="large" onClick={handleDrawerClose} component={Link} to="/dashboard">Home</Button>
                   <Button variant="contained" size="large" onClick={handleDrawerClose} component={Link} to="/guides">Guides</Button>
                   <Button variant="contained" size="large" onClick={handleDrawerClose} component={Link} to="/rewards">Rewards</Button>
+                  <Button variant="contained" size="large" onClick={handleDrawerClose} component={Link} to="/cart">Cart</Button>
+                  <Button variant="contained" size="large" onClick={handleDrawerClose} component={Link} to="/AddProducts">AddProducts</Button>
                 </Stack>
                 </List>
             </Drawer>
