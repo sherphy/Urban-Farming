1070f3ebabcefb739f4c9c5c4cf43986781b44f3
randomgamer800 <73993052+randomgamer800@users.noreply.github.com>
2022-02-18 20:36:22 +0800

    App.js
    added addproducts

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Rewards from "./pages/rewards";
 import Products from "./pages/rewards";
 import Signup from "./pages/signup";
 import Cart from "./pages/cart";
+import AddProducts from "./util/AddProducts"
 //might need to remove later to hide it from view
 // import AddProducts from "./components/AddProducts";
 //utilities
@@ -84,7 +85,7 @@ export class App extends Component {
                 <Route path="/rewards" element={<Products />} />
                 <Route path="/cart" element={<Cart />} />
                 {/* might need to remove later to hide it from view */}
-                {/* <Route path='/addproducts' element={<AddProducts />} /> */}
+                <Route path='/addproducts' element={<AddProducts />} /> 
               </Route>
         
                <Route path="/timer" element={<Timer />} /> {/*for testing/quiz component to be used later*/}
