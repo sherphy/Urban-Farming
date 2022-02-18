dda5864d51678b4b541d8d4b9d5f0210a3169ae9
randomgamer800 <73993052+randomgamer800@users.noreply.github.com>
2022-02-18 20:41:41 +0800

    Update login.js
    Updated with lastlog and currenttime difference (updated login.js code)

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,63 +1,67 @@
-import React, { useState } from "react";
+import React, { useState, createContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { Form, Alert } from "react-bootstrap";
 import { Button } from "react-bootstrap";
 import { useUserAuth } from "../util/userAuthContext";
+import {auth} from '../util/firebase'
 import './login.css'
 
 const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
-  const { logIn } = useUserAuth();
   const navigate = useNavigate();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setError("");
-    try {
-      await logIn(email, password);
-      navigate("/dashboard");
-    } catch (err) {
-      setError(err.message);
-    }
+  const [email, setEmail]=useState('');
+  const [password, setPassword]=useState('');
+  const [errorMsg, setErrorMsg]=useState('');
+  const [successMsg, setSuccessMsg]=useState('');
+
+
+  const handleLogin=(e)=>{
+      e.preventDefault();
+      // console.log(email, password);
+      auth.setPersistence('local');
+      auth.signInWithEmailAndPassword(email,password).then(()=>{
+          setSuccessMsg('Login Successfull. You will now automatically get redirected to Home page');
+          setEmail('');
+          setPassword('');
+          setErrorMsg('');
+          setSuccessMsg('');
+          navigate('/dashboard');
+      }).catch(error=>setErrorMsg(error.message));
   };
 
   return (
-    <>
       <div className="sign">
-        <h2>Login</h2>
-        {error && <Alert variant="danger">{error}</Alert>}
-        <Form onSubmit={handleSubmit}>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Control
-              type="email"
-              placeholder="Email address"
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </Form.Group>
-
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Control
-              type="password"
-              placeholder="Password"
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </Form.Group>
-
-          <div className="d-grid gap-2">
-            <Button variant="primary" type="Submit">
-              Log In
-            </Button>
-          </div>
-        </Form>
-
-      <span>
-        Don't have an account? <Link to="/signup">Sign up</Link>
-      </span>
-      </div>
-    </>
-  );
-};
+      <br></br>
+            <br></br>
+            <h1>Login</h1>
+            <hr></hr>
+            {successMsg&&<>
+                <div className='success-msg'>{successMsg}</div>
+                {navigate('/dashboard')};
+                <br></br>
+            </>}
+            <form className='form-group' autoComplete="off"
+            onSubmit={handleLogin}>               
+                <label>Email</label>
+                <input type="email" className='form-control' required
+                onChange={(e)=>setEmail(e.target.value)} value={email}></input>
+                <br></br>
+                <label>Password</label>
+                <input type="password" className='form-control' required
+                onChange={(e)=>setPassword(e.target.value)} value={password}></input>
+                <br></br>
+                <div className='btn-box'>
+                    <span><Link to="/signup" className='link'>Don't have an account? Sign up!</Link></span>
+                    <br/>
+                    <button type="submit" className='btn btn-success btn-md'>LOGIN</button>
+                </div>
+            </form>
+            {errorMsg&&<>
+                <br></br>
+                <div className='error-msg'>{errorMsg}</div>                
+            </>}
+        </div>
+    )
+}
 
 export default Login; 
