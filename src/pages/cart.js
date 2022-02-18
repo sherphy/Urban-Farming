cfa060d169b540b61bc95a0d2ee84afe31f3f5d5
randomgamer800 <73993052+randomgamer800@users.noreply.github.com>
2022-02-18 20:43:06 +0800

    Update cart.js
    copied over cart.js code from version2-store to my code

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -1,91 +1,147 @@
-import React, { useContext, useEffect } from 'react'
-import { CartContext } from '../util/CartContext'
-// import { Navbar } from '../components/Navbar';
-import { Icon } from 'react-icons-kit'
-import { ic_add } from 'react-icons-kit/md/ic_add'
-import { ic_remove } from 'react-icons-kit/md/ic_remove'
-import { iosTrashOutline } from 'react-icons-kit/ionicons/iosTrashOutline'
-import { Link } from 'react-router-dom'
-import { useNavigate } from 'react-router-dom'
-import { auth } from '../util/firebase'
+import React,{useState, useEffect} from 'react'
+// import Navbar from '../components/Navbar'
+import {auth, db} from '../util/firebase'
+import CartProducts from '../util/CartProducts';
+//if we want to monetize
+// import StripeCheckout from 'react-stripe-checkout';
 
-const Cart = ({ user }) => {
+const Cart = () => {
 
-    const { shoppingCart, dispatch, totalPrice, totalQty } = useContext(CartContext);
+    // getting current user function
+    function GetCurrentUser(){
+        const [user, setUser]=useState(null);
+        useEffect(()=>{
+            auth.onAuthStateChanged(user=>{
+                if(user){
+                    db.collection('SignedUpUsersData').doc(user.uid).get().then(snapshot=>{
+                        setUser(snapshot.data().FullName);
+                    })
+                }
+                else{
+                    setUser(null);
+                }
+            })
+        },[])
+        return user;
+    }
 
-    const history = useNavigate();
+    const user = GetCurrentUser();
+    console.log(user);
+    
+    // state of cart products
+    const [cartProducts, setCartProducts]=useState([]);
 
-    useEffect(() => {
-        auth.onAuthStateChanged(user => {
-            if (!user) {
-                history.push('/login');
+    // getting cart products from firestore collection and updating the state
+    useEffect(()=>{
+        auth.onAuthStateChanged(user=>{
+            if(user){
+                db.collection('Cart ' + user.uid).onSnapshot(snapshot=>{
+                    const newCartProduct = snapshot.docs.map((doc)=>({
+                        ID: doc.id,
+                        ...doc.data(),
+                    }));
+                    setCartProducts(newCartProduct);                    
+                })
+            }
+            else{
+                console.log('user is not signed in to retrieve cart');
             }
         })
+    },[])
+
+    // console.log(cartProducts);
+
+       // getting the qty from cartProducts in a seperate array
+    //    const qty = cartProducts.map(cartProduct=>{
+    //     return cartProduct.qty;
+    // })
+
+    // reducing the qty in a single value
+    // const reducerOfQty = (accumulator, currentValue)=>accumulator+currentValue;
+
+    // const totalQty = qty.reduce(reducerOfQty,0);
+
+    // console.log(totalQty);
+
+    // getting the TotalProductPrice from cartProducts in a seperate array
+    const price = cartProducts.map((cartProduct)=>{
+        return cartProduct.TotalProductPrice;
     })
 
+    // reducing the price in a single value
+    const reducerOfPrice = (accumulator,currentValue)=>accumulator+currentValue;
+
+    const totalPrice = price.reduce(reducerOfPrice,0);
+
+
+    let Product;
+    
+    // cart product increase function
+    const cartProductIncrease=(cartProduct)=>{
+        // console.log(cartProduct);
+        Product=cartProduct;
+        Product.qty=Product.qty+1;
+        Product.TotalProductPrice=Product.qty*Product.price;
+        // updating in database
+        auth.onAuthStateChanged(user=>{
+            if(user){
+                db.collection('Cart ' + user.uid).doc(cartProduct.ID).update(Product).then(()=>{
+                    console.log('increment added');
+                })
+            }
+            else{
+                console.log('user is not logged in to increment');
+            }
+        })
+    }
+
+    // cart product decrease functionality
+    const cartProductDecrease =(cartProduct)=>{
+        Product=cartProduct;
+        if(Product.qty > 1){
+            Product.qty=Product.qty-1;
+            Product.TotalProductPrice=Product.qty*Product.price;
+             // updating in database
+            auth.onAuthStateChanged(user=>{
+                if(user){
+                    db.collection('Cart ' + user.uid).doc(cartProduct.ID).update(Product).then(()=>{
+                        console.log('decrement');
+                    })
+                }
+                else{
+                    console.log('user is not logged in to decrement');
+                }
+            })
+        }
+    }
+   
     return (
         <>
-            {/* <Navbar user={user} /> */}
-            <>
-                {shoppingCart.length !== 0 && <h1>Cart</h1>}
-                <div className='cart-container'>
-                    {
-                        shoppingCart.length === 0 && <>
-                            <div>no items in your cart or slow internet causing trouble (Refresh the page) or you are not logged in</div>
-                            <div><Link to="/">Return to Home page</Link></div>
-                        </>
-                    }
-                    {shoppingCart && shoppingCart.map(cart => (
-                        <div className='cart-card' key={cart.ProductID}>
-
-                            <div className='cart-img'>
-                                <img src={cart.ProductImg} alt="not found" />
-                            </div>
-
-                            <div className='cart-name'>{cart.ProductName}</div>
-
-                            <div className='cart-price-orignal'>Rs {cart.ProductPrice}.00</div>
-
-                            <div className='inc' onClick={() => dispatch({ type: 'INC', id: cart.ProductID, cart })}>
-                                <Icon icon={ic_add} size={24} />
-                            </div>
-
-                            <div className='quantity'>{cart.qty}</div>
-
-                            <div className='dec' onClick={() => dispatch({ type: 'DEC', id: cart.ProductID, cart })}>
-                                <Icon icon={ic_remove} size={24} />
-                            </div>
-
-                            <div className='cart-price'>
-                                Rs {cart.TotalProductPrice}.00
-                            </div>
-
-                            <button className='delete-btn' onClick={() => dispatch({ type: 'DELETE', id: cart.ProductID, cart })}>
-                                <Icon icon={iosTrashOutline} size={24} />
-                            </button>
+            {/* <Navbar user={user} totalProducts={totalProducts} />            */}
+            <br></br>
+            {cartProducts.length > 0 && (
+                <div className='container-fluid'>
+                    <h1 className='text-center'>Cart</h1>
+                    <div className='products-box'>
+                        <CartProducts cartProducts={cartProducts}
+                           cartProductIncrease={cartProductIncrease}
+                           cartProductDecrease={cartProductDecrease}
+                        />
+                    </div>
+                    <div className='summary-box'>
+                        <h5>Cart Summary</h5>
+                        <div>
+                        Total Points Required: <span> {totalPrice} Points </span>
                         </div>
-                    ))
-                    }
-                    {shoppingCart.length > 0 && <div className='cart-summary'>
-                        <div className='cart-summary-heading'>
-                            Cart-Summary
-                        </div>
-                        <div className='cart-summary-price'>
-                            <span>Total Price</span>
-                            <span>{totalPrice}</span>
-                        </div>
-                        <div className='cart-summary-price'>
-                            <span>Total Qty</span>
-                            <span>{totalQty}</span>
-                        </div>
-                        <Link to='cashout' className='cashout-link'>
-                            <button className='btn btn-success btn-md' style={{ marginTop: 5 + 'px' }}>
-                                Cash on delivery
-                        </button>
-                        </Link>
-                    </div>}
+                        <br></br>
+                        {/* process the points here */}
+                        <button> Order now! </button>
+                    </div>                                    
                 </div>
-            </>
+            )}
+            {cartProducts.length < 1 && (
+                <div className='container-fluid'>No products to show</div>
+            ) }           
         </>
     )
 }
