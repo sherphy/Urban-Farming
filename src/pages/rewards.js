5f8f3e1f177e481e399cd5be0726ea239a65a0d7
randomgamer800 <73993052+randomgamer800@users.noreply.github.com>
2022-02-18 20:44:20 +0800

    Update rewards.js
    copied over rewards.js from version2-store

--- src/pages/rewards.js
+++ src/pages/rewards.js
@@ -1,12 +1,20 @@
 //handles the frontend and backend for the reward shop
 import { Typography } from "@material-ui/core";
-import React, { useContext } from "react";
+import React, { useContext,useState, useEffect } from "react";
 import { makeStyles, useTheme } from "@material-ui/styles";
 import useWindowDimensions from "../util/useWindowDimensions";
 import shop from "../images/shop.png";
 import Box from "@material-ui/core/Box";
 import { ProductsContext } from '../util/ProductsContext'
 import { CartContext } from '../util/CartContext'
+import Products from '../util/Product'
+import { auth, db } from '../util/firebase'
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+
+toast.configure();
 
 //frontend
 const useStyles = makeStyles((theme) => ({
@@ -31,11 +39,107 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 //backend
-const Products = () => {
+const Rewards = (props) => {
 
-  const { products } = useContext(ProductsContext);
+      // getting current user uid
+      function GetUserUid() {
+        const [uid, setUid] = useState(null);
+        useEffect(() => {
+            auth.onAuthStateChanged(user => {
+                if (user) {
+                    setUid(user.uid);
+                }
+            })
+        }, [])
+        return uid;
+    }
 
-  const { dispatch } = useContext(CartContext);
+    const uid = GetUserUid();
+
+    // getting current user function
+    function GetCurrentUser() {
+        const [user, setUser] = useState(null);
+        useEffect(() => {
+            auth.onAuthStateChanged(user => {
+                if (user) {
+                    db.collection('SignedUpUsersData').doc(user.uid).get().then(snapshot => {
+                        setUser(snapshot.data().FullName);
+                    })
+                }
+                else {
+                    setUser(null);
+                }
+            })
+        }, [])
+        return user;
+    }
+
+    const user = GetCurrentUser();
+    console.log(user);
+
+    // state of products
+    const [products, setProducts] = useState([]);
+
+    // getting products function
+    const getProducts = async () => {
+        const products = await db.collection('Products').orderBy("price", "asc").get();
+        const productsArray = [];
+        for (var snap of products.docs) {
+            var data = snap.data();
+            data.ID = snap.id;
+            productsArray.push({
+                ...data
+            })
+            if (productsArray.length === products.docs.length) {
+                setProducts(productsArray);
+            }
+        }
+    }
+
+    useEffect(() => {
+        getProducts();
+    }, [])
+
+    let Product;
+    const addToCart = (product) => {
+        if (uid !== null) {
+            console.log(product);
+            Product = product;
+            Product['qty'] = 1;
+            Product['Orderedby'] = user;
+            Product['TotalProductPrice'] = Product.qty * Product.price;
+            db.collection('Cart ' + uid).doc(product.ID).set(Product).then(() => {
+                console.log('successfully added to cart');
+                //success notification procs even when quantity is more than 1 
+                //can uncomment once i find a way to increase qty from rewards itself
+
+                // if (Product.qty !== 1) {
+                    toast.success('This product has been added to your cart', {
+                        position: "top-right",
+                        autoClose: 2000,
+                        hideProgressBar: false,
+                        closeOnClick: true,
+                        pauseOnHover: false,
+                        draggable: false,
+                        progress: undefined,
+                    });
+                // }
+
+                // toast.warn('This product is already in your cart', {
+                //     position: "top-right",
+                //     autoClose: 2000,
+                //     hideProgressBar: false,
+                //     closeOnClick: true,
+                //     pauseOnHover: false,
+                //     draggable: false,
+                //     progress: undefined,
+                // });
+            })
+        }
+        else {
+            props.history.push('/dashboard');
+        }
+    }
 
   //frontend
   const { height, width } = useWindowDimensions();
@@ -44,6 +148,7 @@ const Products = () => {
 
   let stlWidth, stlHeight;
   let imgWidth, imgHeight;
+  let productWidth, productHeight;
   if (width <= 960) {
     stlWidth = 250;
     stlHeight = 250;
@@ -60,29 +165,23 @@ const Products = () => {
     <>
     <div className={classes.container}>
      <img src={shop} alt="Logo" style={{ height: imgHeight, width: imgWidth }}/>
-        {products.length !== 0 && <h1>Reward Shop</h1>}
-        <h3>Redeem your points for real life items here!</h3> 
-        <div className='products-container'>
-            {products.length === 0 && <div>Loading</div>}
-            {products.map(product => (
-                <div className='product-card' key={product.ProductID}>
-                {/* need to add icreasing price view */}
-                    <div className='product-img'>
-                        <img src={product.ProductImg} alt="not found" />
-                    </div>
-                    <div className='product-name'>
-                        {product.ProductName}
-                    </div>
-                    <div className='product-price'>
-                        {product.ProductPrice} points
-                </div>
-                    <button className='addcart-btn' onClick={() => dispatch({ type: 'ADD_TO_CART', id: product.ProductID, product })}>ADD TO CART</button>
-                </div>
-            ))}
-        </div>
+     {products.length > 0 && (
+       <div>
+         <Typography inline variant="h4" align="center" className={classes.bodyText}>Rewards</Typography>
+         <Typography inline variant="h4" align="center" className={classes.bodyText}>Redeem real life items here!</Typography>
+         <div class="lists">
+           <Grid container spacing={1} direction="column" alignItems="center" justify="center">
+               <Products className="item" products={products} addToCart={addToCart}/>
+            </Grid>
+         </div>
+       </div>
+      )}
+      {products.length < 1 && (
+        <div className='container-fluid'>Please wait....</div>
+      )} 
     </div>
     </>
 )
 }
 
-export default Products
+export default Rewards
